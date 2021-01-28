686767113aa99ada3ff2b11ff71c746e0d2a84de
Alex Kowalczuk <39791929+alexkowalczuk@users.noreply.github.com>
2021-01-28 14:13:40 +0100

    Add files via upload
    Completed Hello World in P5

---
+++ README.md
@@ -0,0 +1,11 @@
+## Hello World for P5.js
+#### by Alex Kowalczuk
+January 28th, 2021
+
+
+### Overview
+Simple Hello World text in black box.
+
+### Technical Details
+
+Modify the sketch.js setup() and draw() are used only.
---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+  textSize(40); //font size of our text
+  textAlign(CENTER); //align of the text
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0); //color of the background
+  fill(255, 53, 33); //color of the text
+  text("Hello, World", width/2, height/2); //our text
+}
